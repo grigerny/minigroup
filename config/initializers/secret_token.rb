71ba45fcd914da54671f7a7b8dccbdcc33e7a092
Gary Riger <grigerny@aol.com>
2012-08-30 22:29:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minigroup::Application.config.secret_token = 'd1192f8b57fc4390726daabefa6f34997337e83bec3f5c3d9796c09b47147fc61c03be60fc1ee57b68cd0fd5bcef263189d0697cfc68c193fca2a427d1531a21'
