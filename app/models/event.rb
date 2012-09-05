83d6e87eb0a1c04a27807b9569e32bc5b015863f
Gary Riger <grigerny@aol.com>
2012-09-04 20:53:25 -0400

    ui updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :name, :user_id, :people, :views
+  attr_accessible :name, :user_id, :people, :views, :verb
   
   def pageviews
       self.increment!(:views)
