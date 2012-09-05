a14037982089e57a542f9f193596754d43b68c7b
Gary Riger <grigerny@aol.com>
2012-09-04 23:12:55 -0400

    fixed bugs for acts-as-taggable

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :name, :user_id, :people, :views, :verb, :tag_list
-  acts_as_taggable_on :tags, 
+  attr_accessible :name, :user_id, :people, :views, :verb, :tag_list, :tags
+  acts_as_taggable_on :tags
   
   def pageviews
       self.increment!(:views)
