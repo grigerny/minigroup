93b25d9baa4bd4c494ffd6140abf9fd1cdd2e547
Gary Riger <grigerny@aol.com>
2012-09-10 22:55:06 -0400

    deleted unneeded events:tab attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120911004527) do
+ActiveRecord::Schema.define(:version => 20120911025118) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(:version => 20120911004527) do
     t.string   "verb"
     t.integer  "views"
     t.text     "when"
-    t.string   "tags"
   end
 
   create_table "impressions", :force => true do |t|
