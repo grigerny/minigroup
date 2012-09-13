c175406306f2e1f3a92dbe7d1f004376998bec24
Gary Riger <grigerny@aol.com>
2012-09-12 22:57:34 -0400

    minor enhancements to membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,6 @@ class Membership < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
   
-  attr_accessible :event_id, :user_id
+  attr_accessible :event_id, :user_id, :status
+  validates_uniqueness_of :event_id, :message => "can be only joined once", :scope => 'user_id'
 end
