4a0d82f0ac2d6da0d0241854948a1941bceefb78
Gary Riger <grigerny@aol.com>
2012-09-17 16:51:56 -0400

    made sign-in-ip attribute accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :avatar, :current_sign_in_ip
   # attr_accessible :title, :body
   has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }
   geocoded_by :current_sign_in_ip
